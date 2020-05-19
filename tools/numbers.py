fa1a63cc5e661028a8f2a8d5ff29acb1168554dc
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-19 14:06:27 -0400

    tools.numbers: tools for numbers in different bases

---
+++ tools/numbers.py
@@ -0,0 +1,39 @@
+DEFAULT__ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DEFAULT__BASE = 10
+
+
+def int_as_base(
+    number,
+    base=DEFAULT__BASE,
+    alphabet=DEFAULT__ALPHABET,
+):
+
+    result = ""
+
+    while number:
+
+        result += alphabet[number % base]
+        number //= base
+
+    return result[::-1] or "0"
+
+
+def int_to_str(
+    number,
+    base=DEFAULT__BASE,
+    alphabet=DEFAULT__ALPHABET,
+    fill=None,
+    width=None,
+):
+
+    if fill is None:
+        fill = alphabet[0]
+
+    if width is None:
+        width = ""
+
+    return "{0:{fill}>{width}}".format(
+        int_as_base(number, base=base, alphabet=alphabet),
+        fill=fill,
+        width=width,
+    )
