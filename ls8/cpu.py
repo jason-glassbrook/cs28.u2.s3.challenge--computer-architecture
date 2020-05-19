02d2518ab0a04915bdcca8969fb1bcb6b4f2cba1
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-18 22:07:13 -0400

    ls8.cpu: def BinaryItem{,Sequence} classes to manage binary values

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,11 +1,18 @@
 """CPU functionality."""
 
 import sys
+import math
 
 ############################################################
 
 BIT__COUNT = 8
 BIT__MASK = int(bin((2 ** BIT__COUNT) - 1), base=2)
+
+HEX__COUNT = math.ceil(BIT__COUNT / 4)
+FORMAT = f"0x%0{HEX__COUNT}X"
+
+# OCT__COUNT = math.ceil(BIT__COUNT / 3)
+# FORMAT = f"0o%0{OCT__COUNT}o"
 
 #-----------------------------------------------------------
 
@@ -16,6 +23,75 @@ def mask(value, binary_mask=BIT__MASK):
     return (value & binary_mask)
 
 
+############################################################
+#   "Binary" helper classes
+############################################################
+
+
+class BinaryItem:
+    """A class to manage binary items."""
+
+    def __init__(self, value):
+
+        self.value = value
+        return
+
+    def __str__(self):
+
+        # return (f"0o%0{OCT__COUNT}o" % self.value)
+        return (FORMAT % self.value)
+
+    @property
+    def value(self):
+
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+
+        self.__value = mask(value)
+        return
+
+
+class BinaryItemSequence:
+    """A class to manage sequences of binary items."""
+
+    def __init__(self, sequence):
+
+        self.sequence = sequence
+        return
+
+    def __str__(self):
+
+        return ("[" + ", ".join(str(item) for item in self.sequence) + "]")
+
+    def __len__(self):
+
+        return len(self.sequence)
+
+    def __getitem__(self, key):
+
+        return self.sequence[key]
+
+    def __setitem__(self, key, value):
+
+        self.sequence[key].value = value
+        return
+
+    @property
+    def sequence(self):
+
+        return self.__sequence
+
+    @sequence.setter
+    def sequence(self, sequence):
+
+        self.__sequence = [BinaryItem(value) for value in sequence]
+        return
+
+
+############################################################
+#   CPU
 ############################################################
 
 
@@ -98,7 +174,7 @@ class CPU:
         """
 
         print(
-            f"TRACE --- %02X %02X %02X | %02X %02X %02X |" % (
+            f"TRACE --- {FORMAT} {FORMAT} {FORMAT} | {FORMAT} {FORMAT} {FORMAT} |" % (
                 self.program_pointer,
                 self.stack_pointer,
                 self.flags,
@@ -110,7 +186,7 @@ class CPU:
         )
 
         for address in range(BIT__COUNT):
-            print(" %02X" % self.read_memory(address), end="")
+            print(f" {FORMAT}" % self.read_memory(address), end="")
 
         print()
 
